dc185e744a7167ca99b6a594d3470cbdf82273c7
李佐宁 <lizuoning@yuan-info.com>
2022-06-27 14:59:03 +0800

    fix: test

--- script/release.sh
+++ script/release.sh
@@ -2,3 +2,15 @@
 set -e
 echo "Enter release version: "
 read VERSION
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..." 
+  git add .
+  echo "input commit:"
+  read commit
+  git commit -m "$commit"
+  git push origin master
+  npm publish
+fi
